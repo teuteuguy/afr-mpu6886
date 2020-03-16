7bbc699c4b1ea4678f0e5cd2b7a2e5b2c8582842
Timothee Cruse <tcruse@amazon.com>
2020-03-16 19:20:58 +0800

    First commit

---
+++ include/mpu6886.h
@@ -0,0 +1,182 @@
+/**
+ * mpu6886.h - Component to work with MPU6886
+ *
+ * Include this header file to use the component.
+ *
+ * (C) 2019 - Timothee Cruse <timothee.cruse@gmail.com>
+ * This code is licensed under the MIT License.
+ */
+
+#ifndef _MPU6886_H_
+#define _MPU6886_H_
+
+#include "freertos/FreeRTOS.h"
+#include "iot_i2c.h"
+
+typedef int32_t mpu6886_err_t;
+
+#define MPU6886_I2C_ADDRESS             ( 0x68 )
+
+/**
+ * @brief   MPU6886 FAILURE CODES
+ */
+#define MPU6886_SUCCESS             ( 0 )     /** Operation completed successfully. */
+#define MPU6886_FAIL                ( 1 )     /** Operation failed. */
+#define MPU6886_OTHER_ERROR         ( 2 )     /** Other error. */
+
+
+/**
+ * @brief   MPU6886 FAILURE CODES
+ */
+
+#define MPU6886_REG_XG_OFFS_TC_H        ( 0x04 )
+#define MPU6886_REG_XG_OFFS_TC_L        ( 0x05 )
+#define MPU6886_REG_YG_OFFS_TC_H        ( 0x07 )
+#define MPU6886_REG_YG_OFFS_TC_L        ( 0x08 )
+#define MPU6886_REG_ZG_OFFS_TC_H        ( 0x0A )
+#define MPU6886_REG_ZG_OFFS_TC_L        ( 0x0B )
+
+#define MPU6886_REG_SELF_TEST_X_ACCEL   ( 0x0D )
+#define MPU6886_REG_SELF_TEST_Y_ACCEL   ( 0x0E )
+#define MPU6886_REG_SELF_TEST_Z_ACCEL   ( 0x0F )
+
+#define MPU6886_REG_XG_OFFS_USRH        ( 0x13 )
+#define MPU6886_REG_XG_OFFS_USRL        ( 0x14 )
+#define MPU6886_REG_YG_OFFS_USRH        ( 0x15 )
+#define MPU6886_REG_YG_OFFS_USRL        ( 0x16 ) 
+#define MPU6886_REG_ZG_OFFS_USRH        ( 0x17 )
+#define MPU6886_REG_ZG_OFFS_USRL        ( 0x18 )
+#define MPU6886_REG_SMPLRT_DIV          ( 0x19 )
+
+#define MPU6886_REG_CONFIG              ( 0x1A )
+#define MPU6886_REG_GYRO_CONFIG         ( 0x1B )
+#define MPU6886_REG_ACCEL_CONFIG_1      ( 0x1C )
+#define MPU6886_REG_ACCEL_CONFIG_2      ( 0x1D )
+#define MPU6886_REG_LP_MODE_CFG         ( 0x1E )
+
+#define MPU6886_REG_ACCEL_WOM_X_THR     ( 0x20 )
+#define MPU6886_REG_ACCEL_WOM_Y_THR     ( 0x21 )
+#define MPU6886_REG_ACCEL_WOM_Z_THR     ( 0x22 )
+#define MPU6886_REG_FIFO_EN             ( 0x23 )
+
+#define MPU6886_REG_FSYNC_INT           ( 0x36 )
+
+#define MPU6886_REG_INT_PIN_CFG         ( 0x37 )
+#define MPU6886_REG_INT_ENABLE          ( 0x38 )
+#define MPU6886_REG_FIFO_WM_INT_STATUS  ( 0x39 )
+#define MPU6886_REG_INT_STATUS          ( 0x3A )
+#define MPU6886_REG_ACCEL_XOUT_H        ( 0x3B )
+#define MPU6886_REG_ACCEL_XOUT_L        ( 0x3C )
+#define MPU6886_REG_ACCEL_YOUT_H        ( 0x3D )
+#define MPU6886_REG_ACCEL_YOUT_L        ( 0x3E )
+#define MPU6886_REG_ACCEL_ZOUT_H        ( 0x3F )
+#define MPU6886_REG_ACCEL_ZOUT_L        ( 0x40 )
+#define MPU6886_REG_TEMP_OUT_H          ( 0x41 )
+#define MPU6886_REG_TEMP_OUT_L          ( 0x42 )
+#define MPU6886_REG_GYRO_XOUT_H         ( 0x43 )
+#define MPU6886_REG_GYRO_XOUT_L         ( 0x44 )
+#define MPU6886_REG_GYRO_YOUT_H         ( 0x45 )
+#define MPU6886_REG_GYRO_YOUT_L         ( 0x46 )
+#define MPU6886_REG_GYRO_ZOUT_H         ( 0x47 )
+#define MPU6886_REG_GYRO_ZOUT_L         ( 0x48 )
+
+#define MPU6886_REG_SELF_TEST_X_GYRO    ( 0x50 )
+#define MPU6886_REG_SELF_TEST_Y_GYRO    ( 0x51 )
+#define MPU6886_REG_SELF_TEST_Z_GYRO    ( 0x52 )
+#define MPU6886_REG_E_ID0               ( 0x53 )
+#define MPU6886_REG_E_ID1               ( 0x54 )
+#define MPU6886_REG_E_ID2               ( 0x55 )
+#define MPU6886_REG_E_ID3               ( 0x56 )
+#define MPU6886_REG_E_ID4               ( 0x57 )
+#define MPU6886_REG_E_ID5               ( 0x58 )
+#define MPU6886_REG_E_ID6               ( 0x59 )
+
+#define MPU6886_REG_FIFO_WM_TH1         ( 0x60 )
+#define MPU6886_REG_FIFO_WM_TH2         ( 0x61 )
+
+#define MPU6886_REG_SIGNAL_PATH_RESET   ( 0x68 )
+#define MPU6886_REG_ACCEL_INTEL_CTRL    ( 0x69 )
+#define MPU6886_REG_USER_CTRL           ( 0x6A )
+#define MPU6886_REG_PWR_MGMT_1          ( 0x6B )
+#define MPU6886_REG_PWR_MGMT_2          ( 0x6C )
+
+#define MPU6886_REG_I2C_IF              ( 0x70 )
+#define MPU6886_REG_FIFO_COUNTH         ( 0x72 )
+#define MPU6886_REG_FIFO_COUNTL         ( 0x73 )
+#define MPU6886_REG_FIFO_R_W            ( 0x74 )
+#define MPU6886_REG_WHO_AM_I            ( 0x75 )
+#define MPU6886_REG_XA_OFFSET_H         ( 0x77 )
+#define MPU6886_REG_XA_OFFSET_L         ( 0x78 )
+#define MPU6886_REG_YA_OFFSET_H         ( 0x7A )
+#define MPU6886_REG_YA_OFFSET_L         ( 0x7B )
+#define MPU6886_REG_ZA_OFFSET_H         ( 0x7D )
+#define MPU6886_REG_ZA_OFFSET_L         ( 0x7E )
+
+#define MPU6886_RESET_VALUE_WHO_AM_I    ( 0x19 )
+
+//#define G (9.8)
+#define RtA                         ( 57.324841 )
+#define AtR    	                    ( 0.0174533	)
+#define Gyro_Gr	                    ( 0.0010653 )
+
+enum Ascale {
+	AFS_2G = 0,
+	AFS_4G,
+	AFS_8G,
+	AFS_16G
+};
+
+enum Gscale {
+	GFS_250DPS = 0,
+	GFS_500DPS,
+	GFS_1000DPS,
+	GFS_2000DPS
+};
+
+extern enum Gscale Gyscale;
+extern enum Ascale Acscale;
+
+/**
+ * @brief   Initialize MPU6886
+ * *
+ * @return  MPU6886_SUCCESS success
+ *          MPU6886_FAIL failed
+ */
+mpu6886_err_t eMPU6886Init( void );
+
+/**
+ * @brief   Get accelerometer data
+ *
+ * @param   ax: pointer to X value
+ *          ay: pointer to Y value
+ *          az: pointer to Z value
+ * 
+ * @return  MPU6886_SUCCESS success
+ *          MPU6886_FAIL failed
+ */
+mpu6886_err_t eGetMPU6886AccelData( float * ax, float * ay, float * az );
+
+/**
+ * @brief   Get gyro data
+ *
+ * @param   gx: pointer to X value
+ *          gy: pointer to Y value
+ *          gz: pointer to Z value
+ * 
+ * @return  MPU6886_SUCCESS success
+ *          MPU6886_FAIL failed
+ */
+mpu6886_err_t eGetMPU6886GyroData( float * gx, float * gy, float * gz );
+
+/**
+ * @brief   Get temp data
+ *
+ * @param   t: pointer to temp value
+ * 
+ * @return  MPU6886_SUCCESS success
+ *          MPU6886_FAIL failed
+ */
+mpu6886_err_t eGetMPU6886TempData( float * t );
+
+
+#endif // _MPU6886_H_
---
+++ src/mpu6886.c
@@ -0,0 +1,378 @@
+/**
+ * mpu6886.c - Component to work with MPU6886
+ *
+ * (C) 2020 - Timothee Cruse <timothee.cruse@gmail.com>
+ * This code is licensed under the MIT License.
+ */
+
+#include "mpu6886.h"
+
+#include "common_io_helpers.h"
+#include "MahonyAHRS.h"
+
+static const char *TAG = "mpu6886";
+
+/*-----------------------------------------------------------*/
+
+static IotI2CHandle_t prvI2CHandle = NULL;
+
+#define MPU6886_INIT_REG_COUNT 13
+static const uint8_t prvMPU6886InitRegisterDefaultList[ MPU6886_INIT_REG_COUNT * 2 ] = {
+    MPU6886_REG_PWR_MGMT_1,     ( 0x00 ),
+    MPU6886_REG_PWR_MGMT_1,     ( 0x01 << 7 ),
+    MPU6886_REG_PWR_MGMT_1,     ( 0x01 << 0 ),
+    MPU6886_REG_ACCEL_CONFIG_1, ( 0x10 ),
+    MPU6886_REG_GYRO_CONFIG,    ( 0x18 ),
+    MPU6886_REG_CONFIG,         ( 0x01 ),
+    MPU6886_REG_SMPLRT_DIV,     ( 0x05 ),
+    MPU6886_REG_INT_ENABLE,     ( 0x00 ),
+    MPU6886_REG_ACCEL_CONFIG_2, ( 0x00 ),
+    MPU6886_REG_USER_CTRL,      ( 0x00 ),
+    MPU6886_REG_FIFO_EN,        ( 0x00 ),
+    MPU6886_REG_INT_PIN_CFG,    ( 0x22 ),
+    MPU6886_REG_INT_ENABLE,     ( 0x01 )
+};
+
+enum Gscale Gyscale = GFS_2000DPS;
+enum Ascale Acscale = AFS_8G;
+
+float prvARes, prvGRes;
+
+/*-----------------------------------------------------------*/
+
+void prvGetMPU6886Gres( void );
+void prvGetMPU6886Ares( void );
+
+mpu6886_err_t prvGetMPU6886AccelAdc( int16_t * ax, int16_t * ay, int16_t * az );
+mpu6886_err_t prvGetMPU6886GyroAdc( int16_t * gx, int16_t * gy, int16_t * gz );
+mpu6886_err_t prvGetMPU6886TempAdc( int16_t * t );
+
+/*-----------------------------------------------------------*/
+
+mpu6886_err_t eMPU6886Init( IotI2CHandle_t handle )
+{
+    mpu6886_err_t e = MPU6886_FAIL;
+    prvI2CHandle = handle;
+
+    uint8_t tempdata[ 2 ] = { 0 };
+    uint8_t error_count = 0;
+    uint8_t i;s
+
+    tempdata[ 0 ] = MPU6886_REG_WHO_AM_I;
+    e = eReadI2CBytes( handle, MPU6886_I2C_ADDRESS, 1, &tempdata );
+    if ( e != COMMON_IO_SUCCESS || tempdata[ 0 ] != MPU6886_RESET_VALUE_WHO_AM_I )
+    {
+        IotLogError( "eMPU6886Init: Error reading MPU6886_REG_WHO_AM_I (%#04X)", tempdata );
+        return MPU6886_FAIL;
+    }
+
+    for (i = 0; i < MPU6886_INIT_REG_COUNT * 2; i += 2)
+    {
+        vTaskDelay( 10 / portTICK_PERIOD_MS );
+        
+        tempdata[ 0 ] = prvMPU6886InitRegisterDefaultList[ i ];
+        tempdata[ 1 ] = mpu6886_init_register_default_list[ i + 1 ];
+        e = eWriteI2CBytes( handle, MPU6886_I2C_ADDRESS, 2, &tempdata );
+        if ( e != COMMON_IO_SUCCESS )
+        {
+            IotLogError( "eMPU6886Init: Error writting %#04X register", prvMPU6886InitRegisterDefaultList[ i ] );
+            error_count++;
+        }
+        else
+        {
+            IotLogDebug( "eMPU6886Init: %#04X %#04X", prvMPU6886InitRegisterDefaultList[ i ], prvMPU6886InitRegisterDefaultList[ i + 1 ]);
+        }
+        
+    }
+
+    vTaskDelay( 100 / portTICK_PERIOD_MS );
+    
+    prvGetMPU6886Gres();
+    prvGetMPU6886Ares();
+
+    if (error_count == 0)
+    {
+        IotLogDebug( "eMPU6886Init: MPU6886 initialized" );
+        return MPU6886_SUCCESS;
+    }
+    else
+    {
+        IotLogError( "eMPU6886Init: %d errors found while initializing MPU6886", error_count );
+        return MPU6886_FAIL;
+    }
+}
+
+/*-----------------------------------------------------------*/
+
+void prvGetMPU6886Gres( void )
+{
+    switch ( Gyscale )
+    {
+        // Possible gyro scales (and their register bit settings) are:
+        case GFS_250DPS:
+            prvGRes = 250.0 / 32768.0;
+            break;
+        case GFS_500DPS:
+            prvGRes = 500.0 / 32768.0;
+            break;
+        case GFS_1000DPS:
+            prvGRes = 1000.0 / 32768.0;
+            break;
+        case GFS_2000DPS:
+            prvGRes = 2000.0 / 32768.0;
+            break;
+    }
+}
+
+/*-----------------------------------------------------------*/
+
+void prvGetMPU6886Ares( void )
+{
+    switch ( Acscale )
+    {
+        // Possible accelerometer scales (and their register bit settings) are:
+        // 2 Gs (00), 4 Gs (01), 8 Gs (10), and 16 Gs  (11).
+        // Here's a bit of an algorith to calculate DPS/(ADC tick) based on that 2-bit value:
+        case AFS_2G:
+            prvARes = 2.0 / 32768.0;
+            break;
+        case AFS_4G:
+            prvARes = 4.0 / 32768.0;
+            break;
+        case AFS_8G:
+            prvARes = 8.0 / 32768.0;
+            break;
+        case AFS_16G:
+            prvARes = 16.0 / 32768.0;
+            break;
+    }
+}
+
+/*-----------------------------------------------------------*/
+
+
+mpu6886_err_t eGetMPU6886AccelData( float * ax, float * ay, float * az )
+{
+    int16_t accX = 0;
+    int16_t accY = 0;
+    int16_t accZ = 0;
+    mpu6886_err_t e = prvGetMPU6886AccelAdc( &accX, &accY, &accZ );
+
+    *ax = ( float )accX * aRes;
+    *ay = ( float )accY * aRes;
+    *az = ( float )accZ * aRes;
+
+    return e;
+}
+
+/*-----------------------------------------------------------*/
+
+mpu6886_err_t eGetMPU6886GyroData( float * gx, float * gy, float * gz )
+{
+    int16_t gyroX = 0;
+    int16_t gyroY = 0;
+    int16_t gyroZ = 0;
+    mpu6886_err_t e = prvGetMPU6886GyroAdc( &gyroX, &gyroY, &gyroZ );
+
+    *gx = (float)gyroX * gRes;
+    *gy = (float)gyroY * gRes;
+    *gz = (float)gyroZ * gRes;
+
+    return e;
+}
+
+/*-----------------------------------------------------------*/
+
+mpu6886_err_t eGetMPU6886TempData( float * t )
+
+/*-----------------------------------------------------------*/
+
+mpu6886_err_t prvGetMPU6886AccelAdc( int16_t * ax, int16_t  *ay, int16_t * az )
+{
+    uint8_t buf[ 6 ] = { 0 };
+    buf[ 0 ] = MPU6886_REG_ACCEL_XOUT_H;
+
+    mpu6886_err_t e = eReadI2CBytes( prvI2CHandle, MPU6886_I2C_ADDRESS, buf, 6 );
+
+    *ax = ( ( int16_t )buf[ 0 ] << 8 ) | buf[ 1 ];
+    *ay = ( ( int16_t )buf[ 2 ] << 8 ) | buf[ 3 ];
+    *az = ( ( int16_t )buf[ 4 ] << 8 ) | buf[ 5 ];
+
+    return e;
+}
+
+/*-----------------------------------------------------------*/
+
+mpu6886_err_t prvGetMPU6886GyroAdc( int16_t * gx, int16_t * gy, int16_t * gz )
+{
+    uint8_t buf[ 6 ] = { 0 };
+    buf[ 0 ] = MPU6886_REG_GYRO_XOUT_H;
+
+    mpu6886_err_t e = eReadI2CBytes( prvI2CHandle, MPU6886_I2C_ADDRESS, buf, 6 );
+
+    *gx = ( ( int16_t )buf[ 0 ] << 8 ) | buf[ 1 ];
+    *gy = ( ( int16_t )buf[ 2 ] << 8 ) | buf[ 3 ];
+    *gz = ( ( int16_t )buf[ 4 ] << 8 ) | buf[ 5 ];
+
+    return e;
+}
+
+/*-----------------------------------------------------------*/
+
+mpu6886_err_t prvGetMPU6886TempAdc( int16_t * t )
+{
+    uint8_t buf[ 2 ] = { 0 };
+    buf[ 0 ] = MPU6886_REG_TEMP_OUT_H;
+
+    mpu6886_err_t e = eReadI2CBytes( prvI2CHandle, MPU6886_I2C_ADDRESS, buf, 2 );
+
+    *t = ( ( uint16_t )buf[ 0 ] << 8 ) | buf[ 1 ];
+
+    return e;
+}
+
+/*-----------------------------------------------------------*/
+
+mpu6886_err_t eGetMPU6886AhrsData( float * pitch, float * roll, float * yaw )
+{
+    mpu6886_err_t e = MPU6886_FAIL;
+
+    float accX = 0; 
+    float accY = 0;
+    float accZ = 0;
+
+    float gyroX = 0;
+    float gyroY = 0;
+    float gyroZ = 0;
+
+    e = eGetMPU6886yroData( &gyroX, &gyroY, &gyroZ );
+    if ( e != MPU6886_SUCCESS ) {
+        return e;
+    }
+
+    e = eGetMPU6886AccelData( &accX, &accY, &accZ );
+    if ( e != MPU6886_SUCCESS ) {
+        return e;
+    }
+
+    float deg_to_rad = 0.017453292519943295769236907684886f;
+    MahonyAHRSupdateIMU( gyroX * deg_to_rad,
+                        gyroY * deg_to_rad,
+                        gyroZ * deg_to_rad,
+                        accX, accY, accZ, 
+                        pitch, roll, yaw );
+
+    return e;
+}
+
+/*-----------------------------------------------------------*/
+
+mpu6886_err_t eSetMPU6886FIFOEnable( bool enableflag )
+{
+    mpu6886_err_t e = MPU6886_FAIL;
+    uint8_t regdata[ 2 ] = { 0 };
+
+    if (enableflag)
+    {
+        regdata[ 0 ] = MPU6886_REG_FIFO_EN;
+        regdata[ 1 ] = 0x0C;
+        e = eWriteI2CBytes( prvI2CHandle, MPU6886_I2C_ADDRESS, &regdata, 2 );
+        if ( e != COMMON_IO_SUCCESS )
+        {
+            return e;
+        }
+        
+        regdata[ 0 ] = MPU6886_REG_USER_CTRL;
+        regdata[ 1 ] = 0x40;
+        e = eWriteI2CBytes( prvI2CHandle, MPU6886_I2C_ADDRESS, &regdata, 2 );
+        if ( e != COMMON_IO_SUCCESS )
+        {
+            return e;
+        }
+    }
+    else
+    {
+        regdata[ 0 ] = MPU6886_REG_FIFO_EN;
+        regdata[ 1 ] = 0x00;
+        e = eWriteI2CBytes( prvI2CHandle, MPU6886_I2C_ADDRESS, &regdata, 2 );
+        if ( e != COMMON_IO_SUCCESS )
+        {
+            return e;
+        }
+        regdata[ 0 ] = MPU6886_REG_USER_CTRL;
+        regdata[ 1 ] = 0x00;
+        e = eWriteI2CBytes( prvI2CHandle, MPU6886_I2C_ADDRESS, &regdata, 2 );
+        if (e != COMMON_IO_SUCCESS)
+        {
+            return e;
+        }
+    }
+
+    return e;
+}
+
+/*-----------------------------------------------------------*/
+
+
+mpu6886_err_t eReadMPU6886FIFO( uint8_t * rd_data )
+{
+    rd_data[ 0 ] = MPU6886_REG_FIFO_R_W;
+    return eReadI2CBytes( prvI2CHandle, MPU6886_I2C_ADDRESS, rd_data, 1 );
+}
+
+/*-----------------------------------------------------------*/
+
+
+mpu6886_err_t eReadMPU6886FIFOBuff( uint8_t * rd_data , uint16_t length )
+{
+    rd_data[ 0 ] = MPU6886_REG_FIFO_R_W;
+    return eReadI2CBytes( prvI2CHandle, MPU6886_I2C_ADDRESS, rd_data, length );
+}
+
+/*-----------------------------------------------------------*/
+
+mpu6886_err_t eReadMPU6886FIFOCount( uint16_t * rd_data )
+{
+    uint8_t temp[2] = { MPU6886_REG_FIFO_COUNTH, 0 };
+    mpu6886_err_t e = eReadI2CBytes( prvI2CHandle, MPU6886_I2C_ADDRESS, temp, 2 );
+    *rd_data = temp[ 0 ];
+    *rd_data <<= 8;
+    *rd_data |= temp[ 1 ];
+    return e;
+}
+
+/*-----------------------------------------------------------*/
+
+mpu6886_err_t eSetMPU6886GyroFsr( enum Gscale scale )
+{
+    unsigned char regdata[ 2 ] = { MPU6886_REG_GYRO_CONFIG, ( scale << 3 ) };
+    
+    mpu6886_err_t e = eWriteI2CBytes( prvI2CHandle, MPU6886_I2C_ADDRESS, &regdata, 2 );
+
+    vTaskDelay( 10 / portTICK_PERIOD_MS );
+
+    Gyscale = scale;
+    prvGetMPU6886Gres();
+
+    return e;
+}
+
+/*-----------------------------------------------------------*/
+
+mpu6886_err_t eSetMPU6886AccelFsr( enum Ascale scale )
+{
+    unsigned char regdata[ 2 ] = { MPU6886_REG_ACCEL_CONFIG_1 ( scale << 3 ) };
+    
+    mpu6886_err_t e = eWriteI2CBytes( prvI2CHandle, MPU6886_I2C_ADDRESS, &regdata, 2 );
+    
+    vTaskDelay( 10 / portTICK_PERIOD_MS );
+    
+    Acscale = scale;
+    prvGetMPU6886Ares();
+
+    return e;
+}
+
+/*-----------------------------------------------------------*/
+
+
